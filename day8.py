5623d92e8da1cb2ba84e793a43ebc8c710b38562
taigi100 <contact@taigi100.com>
2024-12-08 10:13:06 +0200

    shorten it

--- day8.py
+++ day8.py
@@ -1,49 +1,41 @@
 # f = open("in.txt", "r")
+f = open("ex.txt", "r")
 import itertools
 
-f = open("ex.txt", "r")
 input = f.read()
 lines = input.splitlines()
-N = len(lines)
-M = len(lines[0])
+N, M = len(lines), len(lines[0])
 antenas = {}
-ant = []
-antenas_cnt = 0
+
 for i in range(N):
     for j in range(M):
         if lines[i][j] != ".":
-            antenas_cnt += 1
-            if lines[i][j] in antenas:
-                antenas[lines[i][j]].append((i, j))
-                ant.append((i, j))
-            else:
-                antenas[lines[i][j]] = [(i, j)]
-                ant.append((i, j))
+            antenas.setdefault(lines[i][j], []).append((i, j))
+
+
+def in_bounds(x, y):
+    return 0 <= x < N and 0 <= y < M
 
 
 def part1():
     antinodes = set()
-    for k, v in antenas.items():
-        for pair in itertools.combinations(antenas[k], 2):
-            l, r = pair[0], pair[1]
-            x1 = (2 * l[0] - r[0], 2 * l[1] - r[1])
-            x2 = (2 * r[0] - l[0], 2 * r[1] - l[1])
-            if x1[0] >= 0 and x1[0] < N and x1[1] >= 0 and x1[1] < M:
-                antinodes.add(x1)
-            if x2[0] >= 0 and x2[0] < N and x2[1] >= 0 and x2[1] < M:
-                antinodes.add(x2)
+    for v in antenas.values():
+        for l, r in itertools.combinations(v, 2):
+            for p in [
+                (2 * l[0] - r[0], 2 * l[1] - r[1]),
+                (2 * r[0] - l[0], 2 * r[1] - l[1]),
+            ]:
+                if in_bounds(*p):
+                    antinodes.add(p)
     return len(antinodes)
 
 
 def part2():
     antinodes = set()
-    for k, v in antenas.items():
-        for pair in itertools.combinations(antenas[k], 2):
-            l, r = pair[0], pair[1]
-            added = True
+    for v in antenas.values():
+        for l, r in itertools.combinations(v, 2):
             multiplier = 1
-            while added:
-                added = False
+            while True:
                 x1 = (
                     multiplier * (l[0] - r[0]) + l[0],
                     multiplier * (l[1] - r[1]) + l[1],
@@ -52,23 +44,11 @@ def part2():
                     multiplier * (r[0] - l[0]) + r[0],
                     multiplier * (r[1] - l[1]) + r[1],
                 )
-                if x1[0] >= 0 and x1[0] < N and x1[1] >= 0 and x1[1] < M:
-                    antinodes.add(x1)
-                    added = True
-                if x2[0] >= 0 and x2[0] < N and x2[1] >= 0 and x2[1] < M:
-                    antinodes.add(x2)
-                    added = True
+                if not any(in_bounds(*p) for p in (x1, x2)):
+                    break
+                antinodes.update(p for p in (x1, x2) if in_bounds(*p))
                 multiplier += 1
-    # for i in range(N):
-    #     for j in range(M):
-    #         if (i, j) in antinodes:
-    #             print("#", end="")
-    #         else:
-    #             print(".", end="")
-    #     print()
-    for p in ant:
-        antinodes.add(p)
-    return len(antinodes)
+    return len(antinodes | set(p for v in antenas.values() for p in v))
 
 
 print(part1())
